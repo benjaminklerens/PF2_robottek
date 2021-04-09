a5a661419f2116ed531df1339630837fe8817edd
KevinLHansen <kevin.lhansen@hotmail.com>
2021-04-09 19:38:43 +0200

    Add PIR functionality to doorServo system

--- PF2/doorServo/doorServo.ino
+++ PF2/doorServo/doorServo.ino
@@ -1,21 +1,37 @@
 #include <Servo.h>
 
 Servo doorServo;
+
 int doorServoPin = 3;
+int PIRPin = A5;
+
 int pos = 180;
 int speed = 1; // Degrees of each movement, i.e. speed of door
 
-void setup() {
-  doorServo.attach(doorServoPin);
+int sensMvmt;
+int noiseThreshold = 100; // Threshold for recognizing PIR trigger
 
+void setup() {
+  Serial.begin(9600);
+  
+  doorServo.attach(doorServoPin);
   doorServo.write(pos);
 }
 
 void loop() {
-  //openDoor();
-  //delay(2000);
-  //closeDoor();
-  //delay(2000);
+  delay(100);
+  readSensor();
+  
+  if (sensMvmt >= noiseThreshold) {
+    openDoor();
+    delay(5000);
+    closeDoor();
+  }
+}
+
+void readSensor() {
+  sensMvmt = analogRead(PIRPin);
+  Serial.println(sensMvmt);
 }
 
 void openDoor() {
